bf31dc5feeb314967dd9904ca32291ae9cd2b5de
JoabTorress <joabtorres28@gmail.com>
2022-11-26 09:27:44 -0300

    Exibindo campos na tela e melhorando a estilização

--- src/App.js
+++ src/App.js
@@ -5,29 +5,54 @@ function App() {
   const [cep, setCep] = useState('')
   const [valorBuscado, setValorBuscado] = useState({})
 
+  
+  
+
   const buscarApi = () => {
     if (cep.length === 8 || (cep.length === 9 && cep.includes('-'))) {
       fetch(`https://viacep.com.br/ws/${cep}/json/`)
       .then(data => data.json())
-      .then(data => setValorBuscado(data))
+      .then(data => {
+        console.log(data)
+         setValorBuscado(data)
+        }).catch(e => console.log(e))
+      
 
-      return
+      
+    }else{
+      alert('Opss, parece que seu cep está inválido. Corrija e tente novamente')
     }
+    
+   
+   
 
-    alert('Opss, parece que seu CEP não é assim. Corrija e tente novamente')
+    
   }
 
-  return (
+   return(
+    
     <div className="App">
       <header>
+        <h1>Pega CEP</h1>
         <input
           placeholder='Digite aqui seu CEP'
           value={cep}
+          type='number'
           onChange={(evento) => setCep(evento.target.value)}
         />
         <button onClick={buscarApi}>Buscar</button>
       </header>
-      { valorBuscado.cep }
+    
+       {valorBuscado?.cep}
+       <br></br>
+       {valorBuscado?.localidade}
+       <br></br>
+       {valorBuscado?.bairro}
+       <br></br>
+       {valorBuscado?.logradouro}
+       <br></br>
+       {valorBuscado?.complemento}
+
     </div>
   );
 }
